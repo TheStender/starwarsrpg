697ac07dac5bb0c1cebd5a8595d435ed24233a68
Anthony <anthonymstender@gmail.com>
2021-02-13 22:27:33 -0600

    Added resources

--- src/components/Resources.js
+++ src/components/Resources.js
@@ -7,6 +7,9 @@ class Resources extends Component {
         <h2>Resources</h2>
         <br />
         <p>Here you will find various resources to use during the game</p>
+
+        <br />
+        <p><a href="http://www.legendsofthegalaxy.com/Oggdude/">Oggdude</a> - Here you can download a tool to help you build your character, it's about 137MB though. I will be using this to show how to build a character. It also has good info on a lot of other stuff including gear and ships</p>
       </div>
     );
   }
