ada4f8f3cf98bae65f5091b868d6e13822982859
Anthony <anthonymstender@gmail.com>
2021-02-17 22:03:48 -0600

    Added dice roller link

--- src/components/Resources.js
+++ src/components/Resources.js
@@ -12,6 +12,8 @@ class Resources extends Component {
         <p><a href="http://www.legendsofthegalaxy.com/Oggdude/">Oggdude</a> - Here you can download a tool to help you build your character, it's about 137MB though. I will be using this to show how to build a character. It also has good info on a lot of other stuff including gear and ships</p>
         <br />
         <p><a href="https://images-cdn.fantasyflightgames.com/ffg_content/StarWarsRPG/edge-of-the-empire/support/edge-of-the-empire-character-sheet.pdf">Character Sheet</a> - Link to a PDF Character Sheet</p>
+        <br />
+        <p><a href="https://rpg-dice-roller.herokuapp.com/star_wars">Dice Roller</a> - A dice roller you can use</p>
       </div>
     );
   }
