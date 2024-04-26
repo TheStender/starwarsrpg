45358c760fb854643ed20f02107e8c1c89af82ac
Anthony Stender <anthony.m.stender@gmail.com>
2024-04-26 15:29:18 -0500

    added boost dice to roller

--- js/scripts.js
+++ js/scripts.js
@@ -68,9 +68,9 @@ function rollProficiencyDice(quantity) {
   let totalAdvantages = 0;
   let totalTriumphs = 0;
 
-  // Roll the specified quantity of ability dice
+  // Roll the specified quantity of proficiency dice
   for (let i = 0; i < quantity; i++) {
-    // Roll the ability die (an 8-sided die)
+    // Roll the proficiency die (a 12-sided die)
     const rollResult = Math.floor(Math.random() * 8) + 1;
 
     // Determine the result based on the roll
@@ -114,6 +114,53 @@ function rollProficiencyDice(quantity) {
   };
 }
 
+// Function to roll the boost dice
+function rollBoostDice(quantity) {
+  let totalSuccesses = 0;
+  let totalAdvantages = 0;
+
+  // Roll the specified quantity of boost dice
+  for (let i = 0; i < quantity; i++) {
+    // Roll the boost die (an 6-sided die)
+    const rollResult = Math.floor(Math.random() * 8) + 1;
+
+    // Determine the result based on the roll
+    switch (rollResult) {
+      case 1:
+      case 2:
+        break;
+      case 3:
+        totalAdvantages += 2;
+        break;
+      case 4:
+        totalAdvantages++;
+        break;
+      case 5:
+        totalSuccesses++;
+        totalAdvantages++;
+        break;
+      case 6:
+        totalSuccesses++;
+        break;
+    }
+    console.log("Roll of boost dice " + i + " was " + rollResult);
+  }
+
+  // Return the result as an object
+  return {
+    totalSuccesses: totalSuccesses,
+    totalAdvantages: totalAdvantages
+  };
+}
+
+// Function to roll the difficulty dice
+
+// Function to roll the challenge dice
+
+// Function to roll the setback dice
+
+// Function to roll the force dice
+
 // Function to roll all selected dice and calculate totals
 function rollAllDice() {
   let totalSuccesses = 0;
@@ -135,7 +182,17 @@ function rollAllDice() {
         totalAdvantages += proficiencyResult.totalAdvantages;
         totalTriumphs += proficiencyResult.totalTriumphs
         break;
-      // Add cases for other types of dice as needed
+      case 'boost':
+        const boostResult = rollBoostDice(selectedDice[diceType]);
+        totalSuccesses += boostResult.totalSuccesses;
+        totalAdvantages += boostResult.totalAdvantages;
+        break;
+      case 'difficulty':
+        break;
+      case 'challenge':
+        break;
+      case 'setback':
+        break;
     }
   }
 
