f3655e2f05d581d1d062e53029053ca3be3ab97b
Anthony Stender <anthony.m.stender@gmail.com>
2024-05-16 22:24:52 -0500

    Updated session info and characters

--- js/scripts.js
+++ js/scripts.js
@@ -429,3 +429,19 @@ function resetDicePool() {
   document.getElementById("result").innerText = "";
   console.clear();
 }
+
+document.addEventListener('DOMContentLoaded', (event) => {
+  const collapsibles = document.querySelectorAll('.collapsible');
+  collapsibles.forEach(collapsible => {
+      collapsible.addEventListener('click', function() {
+          this.classList.toggle('active');
+          const content = this.nextElementSibling;
+          if (content.style.display === 'block') {
+              content.style.display = 'none';
+          } else {
+              content.style.display = 'block';
+          }
+      });
+  });
+});
+
